1fb5af7fc0d4e7287f5435453a2b0ab369fbf3e9
Arao <rodrigoanguaya@hotmail.com>
2020-07-26 16:09:32 -0500

    Actualizado

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
 $destino="rodrigoanguaya@hotmail.com";
-$nombre=$_Post["nombre"];
-$correo=$_Post["correo"];
-$telefono=$_Post["telefono"];
-$mensaje=$_Post["mensaje"];
+$nombre=$_Post['nombre'];
+$correo=$_Post['correo'];
+$telefono=$_Post['telefono'];
+$mensaje=$_Post['mensaje'];
 $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
 mail($destino,"Contacto",$contenido);
 header("Location:gracias.html");

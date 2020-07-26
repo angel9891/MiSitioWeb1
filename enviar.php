b46b36074545da59c40ff76f200b0d952572bdc8
Arao <rodrigoanguaya@hotmail.com>
2020-07-26 17:39:07 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
 $destino="fisicamatematicajm@hotmail.com";
-$nombre=$_Post["nombre"];
-$correo=$_Post["correo"];
-$telefono=$_Post["telefono"];
-$mensaje=$_Post["mensaje"];
+$nombre=$_Post['nombre'];
+$correo=$_Post['correo'];
+$telefono=$_Post['telefono'];
+$mensaje=$_Post['mensaje'];
 $contenido="Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
 mail($destino,"Contacto",$contenido);
 header("Location:gracias.html");
